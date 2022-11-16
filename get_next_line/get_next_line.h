312fb8b607edab1af80639bc984eee2c367de1be
Tharunthorn <Tharunthornmusik1446@gmail.com>
2022-11-16 16:57:43 +0700

    changedir

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,12 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thmusik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/16 15:50:56 by thmusik           #+#    #+#             */
-/*   Updated: 2022/11/16 15:51:58 by thmusik          ###   ########.fr       */
+/*   Created: 2022/11/16 15:51:50 by thmusik           #+#    #+#             */
+/*   Updated: 2022/11/16 16:28:45 by thmusik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
